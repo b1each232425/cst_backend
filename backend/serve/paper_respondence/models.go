3628d9f7e9b479c5c8366557d1ad74bfdc09d09c
OuYang Haobin <1242968386@qq.com>
2025-07-18 20:38:41 +0800

    作答模块初始化

---
+++ backend/serve/paper_respondence/models.go
@@ -0,0 +1,37 @@
+package paper_respondence
+
+import "encoding/json"
+
+// SaveOrUpdateStudentAnswerReq   保存学生答题请求
+type SaveOrUpdateStudentAnswerReq struct {
+	Id                 int64           `json:"id"`
+	ExamineeID         int64           `json:"examinee_id" `
+	PracticeSubmission int64           `json:"practice_submission" `
+	Type               string          `json:"type" validate:"required,oneof=00 02"`
+	QuestionID         int64           `json:"question_id" validate:"required"`
+	Answer             json.RawMessage `json:"answer" validate:"required"`
+	StudentId          int64           `json:"student_id" validate:"required"`
+	AttachmentPaths    json.RawMessage `json:"attachment_paths"`
+}
+
+type GetStudentAnswerReq struct {
+	ExamineeID         int64 `json:"examinee_id" `
+	PracticeSubmission int64 `json:"practice_submission" `
+	QuestionID         int64 `json:"question_id" validate:"required"`
+}
+
+type SaveBeginTimeReq struct {
+	Type                 string `json:"type" validate:"required,oneof=00 02"`
+	ExamId               int64  `json:"exam_id" `
+	ExamineeID           int64  `json:"examinee_id" `
+	PracticeSubmissionID int64  `json:"practice_submission_id" `
+	StudentId            int64  `json:"student_id" validate:"required"`
+}
+
+type SubmitReq struct {
+	Type                 string `json:"type" validate:"required,oneof=00 02"`
+	ExamId               int64  `json:"exam_id" `
+	ExamineeID           int64  `json:"examinee_id" `
+	PracticeSubmissionID int64  `json:"practice_submission_id" `
+	StudentId            int64  `json:"student_id" validate:"required"`
+}
