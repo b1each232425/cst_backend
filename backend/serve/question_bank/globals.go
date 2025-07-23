dcc0c3203d4ea7bb2230a8d0a8804a40c265d88f
cpff <3410304292@qq.com>
2025-07-24 00:58:40 +0800

    第一期题库管理所有接口

---
+++ backend/serve/question_bank/globals.go
@@ -0,0 +1,30 @@
+package question_bank
+
+import "w2w.io/null"
+
+const (
+
+	// Question bank types
+	QuestionBankTypeTheory = "00" // 理论题库
+	QuestionBankTypeCoding = "02" // 编程题库
+)
+
+const (
+	GlobalLevelSign  = "00" // 全局权限
+	NormalLevelSign  = "02" // 普通权限
+	CurrentLevelSign = "00" // 当前权限(TODO: 目前未使用, 可能会在后续版本中使用, 仅作为占位符保留)
+)
+
+var QuestionTypes = map[string]string{
+	"00": "单选题",
+	"02": "多选题",
+	"04": "判断题",
+	"06": "填空题",
+	"08": "简答题",
+}
+
+var QuestionDifficulty = map[null.Int]string{
+	null.IntFrom(1): "简单",
+	null.IntFrom(2): "中等",
+	null.IntFrom(3): "困难",
+}
