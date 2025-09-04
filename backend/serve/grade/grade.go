b23da5f6254d492f684d03ee60f857131b1413f7
lly <3102128343@qq.com>
2025-09-04 09:43:40 +0800

    修复bug,新增测试. Related to knowledge-training/exam#103

--- backend/serve/grade/grade.go
+++ backend/serve/grade/grade.go
@@ -335,7 +335,9 @@ func gradeSubmissionH(ctx context.Context) {
 	method := strings.ToLower(q.R.Method)
 	switch method {
 	case "patch":
+
 		var err error
+
 		var buf []byte
 		buf, err = io.ReadAll(q.R.Body)
 		if forceErr == "io.ReadAll fail" {
