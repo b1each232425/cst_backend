9fc5eb1d81191b09cbefa6b392dc5c4fb424f69f
lly <3102128343@qq.com>
2025-09-06 10:54:38 +0800

    修复学生端返回报名计划bug. Related to knowledge-training/exam#103

--- backend/serve/registration/dao.go
+++ backend/serve/registration/dao.go
@@ -195,7 +195,7 @@ func ListRegisterS(ctx context.Context, name string, course string, status strin
 	clauses = append(clauses, fmt.Sprintf("r.start_time < $%d", len(args)+1))
 	args = append(args, time.Now().UnixMilli())
 	s := `
-	SELECT r.id, r.name ,r.review_end_time, r.start_time , r.end_time , r.course , r.exam_plan_location,eps.exam_type , COALESCE(eps.status,'')
+	SELECT r.id, r.name ,r.review_end_time, r.start_time , r.end_time , r.course , r.exam_plan_location,eps.exam_type , eps.status
 	FROM assessuser.t_register_plan r LEFT JOIN  assessuser.t_exam_plan_student eps ON eps.register_id = r.id AND eps.student_id = $1 
 		`
 	if len(clauses) > 0 {
